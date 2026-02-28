334eb451b5d51a38854d7edbaacc95624deb5093
dev0aj <dev0.aj@proton.me>
2026-02-28 14:26:22 +0530

    wezterm: created basic configwq

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+wezterm.on("gui-startup", function(cmd)
+  local mux = wezterm.mux
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+config.automatically_reload_config = true
+
+config.font = wezterm.font 'CodeNewRoman Nerd Font'
+config.color_scheme = 'Tokyo Night Moon'
+
+config.adjust_window_size_when_changing_font_size = true
+
+config.enable_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = false
+return config
