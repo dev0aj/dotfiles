ed3c1da080d9ac82fa52a7612798ed43b00085fb
dev0aj <dev0.aj@proton.me>
2026-02-28 22:31:36 +0530

    added auto session plugin

--- nvim/.config/nvim/lua/plugins/utils.lua
+++ nvim/.config/nvim/lua/plugins/utils.lua
@@ -23,7 +23,49 @@ return {
     "nvim-mini/mini.nvim",
     config = function()
       require("mini.surround").setup()
+      require("mini.pairs").setup()
     end,
   },
+
+  {
+    "rmagatti/auto-session",
+    lazy = false,
+    keys = {
+        -- Will use Telescope if installed or a vim.ui.select picker otherwise
+        { "<leader>wf", "<cmd>AutoSession search<CR>", desc = "Session Find" },
+        { "<leader>ws", "<cmd>AutoSession save<CR>", desc = "Save session" },
+        { "<leader>wa", "<cmd>AutoSession toggle<CR>", desc = "Toggle autosave" },
+    },
+
+    config = function()
+        require("auto-session").setup({
+            log_level = "error",
+            auto_save_enabled = true,
+            auto_restore_enabled = true,
+            auto_session_use_git_branch = true,
+            auto_restore_lazy_delay_enabled = true,
+
+            session_lens = {
+                -- If load_on_setup is false, make sure you use `:SessionSearch` to open the picker as it will initialize everything first
+                load_on_setup = true,
+                previewer = false,
+                mappings = {
+                    -- Mode can be a string or a table, e.g. {"i", "n"} for both insert and normal mode
+                    delete_session = { { "n" }, "dd" },
+                    alternate_session = { { "n" }, "ss" },
+                },
+                -- Can also set some Telescope picker options
+                theme_conf = {
+                    border = true,
+                    layout_config = {
+                        width = 0.8,
+                        height = 0.4,
+                    },
+                    preview = false
+                },
+            },
+        })
+    end,
+  }
 
 }
