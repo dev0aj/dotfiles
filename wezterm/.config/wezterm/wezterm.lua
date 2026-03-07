e407d3dc93de408876d7446c66407256ba281328
dev0aj <dev0.aj@proton.me>
2026-03-07 12:52:06 +0530

    minor changes to wezterm and tmux

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,18 +1,18 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
 config.term = "xterm-256color"
 
 wezterm.on("gui-startup", function(cmd)
-  local mux = wezterm.mux
-  local tab, pane, window = mux.spawn_window(cmd or {})
-  window:gui_window():maximize()
+	local mux = wezterm.mux
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
 end)
 
 config.automatically_reload_config = true
 
-config.font = wezterm.font 'CodeNewRoman Nerd Font'
-config.color_scheme = 'Tokyo Night Moon'
+config.font = wezterm.font("CodeNewRoman Nerd Font")
+config.color_scheme = "Tokyo Night Moon"
 
 config.adjust_window_size_when_changing_font_size = true
 
