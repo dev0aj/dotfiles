0631e3cce1114ffdca86e2a172c7b5fdbbccb388
dev0aj <dev0.aj@proton.me>
2026-04-11 13:14:50 +0530

    removed list config from blink

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -20,12 +20,6 @@ return {
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
 		},
-		list = {
-			selection = {
-				preselect = false,
-				auto_insert = false,
-			},
-		},
 		signature = { enabled = true },
 
 		keymap = {
